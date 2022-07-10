ce24723929ff4720a726db8289f3453132814215
minal1593 <minalsalunkhe15@gmail.com>
2022-07-10 22:21:11 +0530

    Added : Program of linked list uc1 added

---
+++ src/com/Bridgelabz/DataStructure/Node.java
@@ -0,0 +1,19 @@
+package com.Bridgelabz.DataStructure;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", next=" + next +
+                '}';
+    }
+}
