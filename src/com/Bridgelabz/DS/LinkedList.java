e6397139bbb8c12dc8360b06abd76fd8e40b656c
minal1593 <minalsalunkhe15@gmail.com>
2022-07-11 16:08:47 +0530

    Added : Program of Linked List Uc2

---
+++ src/com/Bridgelabz/DS/LinkedList.java
@@ -0,0 +1,52 @@
+package com.Bridgelabz.DS;
+
+public class LinkedList
+{
+    Node head;
+    Node tail;
+
+    public Node push(int data)
+    {
+        Node newNode = new Node(data);
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        } else
+        {
+            Node temp = head;
+            this.head = newNode;
+            newNode.next = temp;
+        }
+        return newNode;
+    }
+
+    public void print()
+    {
+        if (head == null)
+        {
+            System.out.println("Linked List is Empty");
+        }
+        else
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                System.out.print(temp.data+ " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
---
+++ src/com/Bridgelabz/DS/Operations.java
@@ -0,0 +1,24 @@
+package com.Bridgelabz.DS;
+
+public class Operations {
+
+        public static void addDataAtStart()
+        {
+            LinkedList linkedList = new LinkedList();
+            linkedList.push(70);
+            linkedList.push(30);
+            linkedList.push(56);
+            linkedList.print();
+        }
+
+
+        public static void main(String[] args){
+            Operations os=new Operations();
+            os.addDataAtStart();
+
+
+    }
+
+    }
+
+
